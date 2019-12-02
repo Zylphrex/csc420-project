34fbca31539a8373bf23c153d60aad681ecd570d
Jiogit <43357169+Jiogit@users.noreply.github.com>
2019-12-01 23:14:53 -0500

    Working implementation of homography and warp

--- algorithm/homography.py
+++ algorithm/homography.py
@@ -32,27 +32,51 @@ def find_homography(src_points, dst_points):
     y4_prime = dst_points[3][1]
 
     p = []
-    p.append([-1 * x1, -1 * y1, -1, 0, 0, 0, x1 * x1_prime, y1 * x1_prime, x1_prime])
-    p.append([0, 0, 0, -1 * x1, -1 * y1, -1, x1 * y1_prime, y1 * y1_prime, y1_prime])
 
-    p.append([-1 * x2, -1 * y2, -1, 0, 0, 0, x2 * x2_prime, y2 * x2_prime, x2_prime])
-    p.append([0, 0, 0, -1 * x2, -1 * y2, -1, x2 * y2_prime, y2 * y2_prime, y2_prime])
+    p.append([x1, y1, 1, 0, 0, 0, -1 * x1 * x1_prime, -1 * y1 * x1_prime, -1 * x1_prime])
+    p.append([0, 0, 0, x1, y1, 1, -1 * x1 * y1_prime, -1 * y1 * y1_prime, -1 * y1_prime])
 
-    p.append([-1 * x3, -1 * y3, -1, 0, 0, 0, x3 * x3_prime, y3 * x3_prime, x3_prime])
-    p.append([0, 0, 0, -1 * x3, -1 * y3, -1, x3 * y3_prime, y3 * y3_prime, y3_prime])
+    p.append([x2, y2, 1, 0, 0, 0, -1 * x2 * x2_prime, -1 * y2 * x2_prime, -1 * x2_prime])
+    p.append([0, 0, 0, x2, y2, 1, -1 * x2 * y2_prime, -1 * y2 * y2_prime, -1 * y2_prime])
 
-    p.append([-1 * x4, -1 * y4, -1, 0, 0, 0, x4 * x4_prime, y4 * x4_prime, x4_prime])
-    p.append([0, 0, 0, -1 * x4, -1 * y4, -1, x4 * y4_prime, y4 * y4_prime, y4_prime])
+    p.append([x3, y3, 1, 0, 0, 0, -1 * x3 * x3_prime, -1 * y3 * x3_prime, -1 * x3_prime])
+    p.append([0, 0, 0, x3, y3, 1, -1 * x3 * y3_prime, -1 * y3 * y3_prime, -1 * y3_prime])
 
-    p.append([0, 0, 0, 0, 0, 0, 0, 0, 1])
+    p.append([x4, y4, 1, 0, 0, 0, -1 * x4 * x4_prime, -1 * y4 * x4_prime, -1 * x4_prime])
+    p.append([0, 0, 0, x4, y4, 1, -1 * x4 * y4_prime, -1 * y4 * y4_prime, -1 * y4_prime])
 
-    b = [0, 0, 0, 0, 0, 0, 0, 0, 1]
-
-    x = solve(p, b)
-
-    h = []
-    h.append([x[0], x[1], x[2]])
-    h.append([x[3], x[4], x[5]])
-    h.append([x[6], x[7], x[8]])
+    p = np.asarray(p)
+    U, S, V = np.linalg.svd(p)
+    L = V[-1,:] / V[-1,-1]
+    h = L.reshape(3, 3)
 
     return h
+
+def warp_image(img, h, target):
+    target_w = target[0]
+    target_h = target[1]
+
+    h = cv.invert(h)[1]   
+
+    h_11 = h[0][0]
+    h_12 = h[0][1]
+    h_13 = h[0][2]
+
+    h_21 = h[1][0]
+    h_22 = h[1][1]
+    h_23 = h[1][2]
+
+    h_31 = h[2][0]
+    h_32 = h[2][1]
+    h_33 = h[2][2]
+    
+    warped_image = np.zeros((target_h, target_w, 3), dtype=img.dtype)
+    for y in range(target_h):
+        for x in range(target_w):
+            m = np.array([x, y, 1])
+            q = h.dot(m)
+            x_old = int(round(q[0]/q[2]))
+            y_old = int(round(q[1]/q[2]))
+            warped_image[y][x] = img[y_old][x_old]
+
+    return warped_image
